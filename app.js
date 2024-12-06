99d5fbfba5c7e470a8457baa5bd0093c01b9b49a
Isaias Gutierrez <isaias.gutierrez48@gmail.comm>
2024-12-06 13:23:39 -0800

    fixed more sizing issues and implemented dark theme which utilizes cookies

---
+++ app.js
@@ -0,0 +1,18 @@
+const themeButton = document.querySelector("#theme-switch");
+const body = document.body;
+
+
+
+if(localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme')
+}
+
+
+
+themeButton.addEventListener('click', () => {
+    body.classList.toggle('dark-theme')
+
+    const theme = document.body.classList.contains('dark-theme')? 'dark': 'light';
+    localStorage.setItem('theme', theme);
+})
+

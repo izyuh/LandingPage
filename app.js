c9e7feb53aad9e51b65ecf85cc6620988b446200
Isaias Gutierrez <isaias.gutierrez48@gmail.comm>
2024-12-12 13:44:14 -0800

    minor touch-ups

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const themeButton = document.querySelector("#theme-switch");
 const body = document.body;
 
-
-
-if(localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme')
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-theme");
 }
 
+themeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
 
-
-themeButton.addEventListener('click', () => {
-    body.classList.toggle('dark-theme')
-
-    const theme = document.body.classList.contains('dark-theme')? 'dark': 'light';
-    localStorage.setItem('theme', theme);
-})
-
+  const theme = document.body.classList.contains("dark-theme")
+    ? "dark"
+    : "light";
+  localStorage.setItem("theme", theme);
+});
